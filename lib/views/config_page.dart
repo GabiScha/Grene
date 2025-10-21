610d628e6cb7533b741d55b8eff342bab9356423
maisagontijoo <maisaagontijo@gmail.com>
2025-10-21 17:23:24 -0300

    config page

--- lib/views/config_page.dart
+++ lib/views/config_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-//Página de Configurações
+import 'package:google_fonts/google_fonts.dart';
+import '../widgets/config_button.dart';
+import '../widgets/config_icon.dart';
 
 class ConfigPage extends StatefulWidget {
   const ConfigPage({super.key});
@@ -14,41 +15,99 @@ class _ConfigPageState extends State<ConfigPage> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-
-
-
-        //PC
-
-
-
+        // Layout para PC
         if (constraints.maxWidth > 800) {
           return Scaffold(
-
-            //front aq
-
+            backgroundColor: const Color(0xFFF5F5DC),
+            body: Row(
+              children: [
+                const SizedBox(width: 150),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const ConfigIcon(),
+                    const SizedBox(height: 30),
+                    ConfigButton(
+                      text: 'Versão do aplicativo',
+                      onPressed: () {},
+                    ),
+                    ConfigButton(
+                      text: 'Modo Claro/Escuro',
+                      onPressed: () {},
+                    ),
+                    ConfigButton(
+                      text: 'Termos de Uso',
+                      onPressed: () {},
+                    ),
+                    ConfigButton(
+                      text: 'Central de ajuda/Fale Conosco',
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 60),
+                Expanded(
+                  child: Container(
+                    height: 500,
+                    margin: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFCDE7D8),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 100),
+              ],
+            ),
           );
-    }
+        }
 
-
-
-
-
-
-    else {
-
-
-
-      //MOBILE
-
-
-
-      return Scaffold(
-
-            //front aq
-
-      );
-    }
+        // Layout para Mobile
+        else {
+          return Scaffold(
+            backgroundColor: const Color(0xFFF7F5DC),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const ConfigIcon(),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Configurações',
+                        style: GoogleFonts.quicksand(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 30, 
+                          color: Colors.black87, 
+                        ),
+                      ),
+                      const SizedBox(height: 9),
+                      ConfigButton(
+                        text: 'Versão do aplicativo',
+                        onPressed: () {},
+                      ),
+                      ConfigButton(
+                        text: 'Modo Claro/Escuro',
+                        onPressed: () {},
+                      ),
+                      ConfigButton(
+                        text: 'Termos de Uso',
+                        onPressed: () {},
+                      ),
+                      ConfigButton(
+                        text: 'Central de ajuda/Fale Conosco',
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+      },
+    );
   }
-  );
- }
 }
---
+++ lib/widgets/config_icon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ConfigIcon extends StatelessWidget {
+  const ConfigIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 140,
+      height: 140,
+      decoration: BoxDecoration(
+        color: const Color(0xFF40985E), 
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          ClipRRect(
+            child: Image.asset(
+              "lib/assets/img/engrenagem.png",
+              width: 140,
+              height: 140,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
